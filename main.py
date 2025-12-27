f50dce2265227d0eb7f41708b6c9252d621b6a54
tgaiml2025-max <tgaiml2025@gmail.com>
2025-12-27 10:58:58 +0530

    Data Transformation Component

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 
 from networksecurity.component.data_ingestion import DataIngestion
 from networksecurity.component.data_validation import DataValidation
+from networksecurity.component.data_transformation import DataTransformation
 
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
-from networksecurity.entity.config_entity import DataIngetionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngetionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -28,6 +29,13 @@ if __name__=='__main__':
         logging.info('Data Validation Completed')
         print(data_validation_artifact)
 
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data Transformation Started")
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation Completed")
+        
         
         
         
